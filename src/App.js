29b5b478e88fa37fbd1c9533ebb278fd7ff302dd
amannitp <amannitp272@gmail.com>
2022-07-22 22:24:16 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import React from "react";
 import Banner from "./components/Banner";
 import Navbar from "./components/Navbar";
 import Movielist from './components/Movielist';
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Favourite from './components/Favourite';
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    <Banner/>
-    <Movielist/>
-    </>
+    <BrowserRouter>
+     <Navbar/>
+     <Routes>
+        <Route path="/" element={<>
+        <Banner/>
+        <Movielist/>
+        </>}
+
+        />
+        <Route path="/favourites" element={<Favourite/>}
+        />
+     </Routes>
+    </BrowserRouter>
 
     // <React.Fragment></React.Fragment>
   );
---
+++ src/components/Favourite.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Favourite extends Component {
+  render() {
+    return (
+      <div><h1>hello this is my favourite</h1></div>
+    )
+  }
+}
+
+export default Favourite
