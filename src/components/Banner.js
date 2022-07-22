fa33453f6e068cac489a6f97edd7acd139a8cb4d
amannitp <amannitp272@gmail.com>
2022-07-22 21:53:29 +0530

    make banner background dynamic

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
-
+import {movies} from '../movieData'
 export class Banner extends Component {
   render() {
+    let movieElem=movies.results[Math.floor(Math.random()*10)]
+    let backDrop=movieElem.backdrop_path
+    let tittle =movieElem.title
+    let description=movieElem.overview
     return (
         <div className="card banner-card" >
-        <img src="https://static1.colliderimages.com/wordpress/wp-content/uploads/slice_inception_movie_poster_banner_01.jpg?q=50&fit=contain&w=767&h=&dpr=1.5" className="card-img-top banner-img" alt="..."/>
-          <h5 className="card-title banner-tittle">Inception</h5>
-          <p className="card-text banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <img src={`https://image.tmdb.org/t/p/original
+${backDrop}`} className="card-img-top banner-img" alt="..."/>
+          <h5 className="card-title banner-tittle">{tittle}</h5>
+          <p className="card-text banner-text">{description}</p>
       </div>
     )
   }
