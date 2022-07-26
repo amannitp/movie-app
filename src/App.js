116b381fd63f3a4e8886469c5c6787f250296a36
amannitp <amannitp272@gmail.com>
2022-07-26 22:37:00 +0530

    wrap banner and movielist in Route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import './App.css'
+import "./App.css";
 import React from "react";
 import Banner from "./components/Banner";
 import Navbar from "./components/Navbar";
-import Movielist from './components/Movielist';
-import { BrowserRouter,Routes} from 'react-router-dom';
-import Favourite from './components/Favourite';
+import Movielist from "./components/Movielist";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Favourite from "./components/Favourite";
 
 function App() {
   return (
-    
     <BrowserRouter>
-         <Navbar/>
-         <Routes>
-         <Banner/>
-         <Movielist/>
-         <Favourite/>
-         </Routes>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner />
+              <Movielist />
+            </>
+          }
+        />
+        <Route path="/favourates" element={<Favourite />} />
+      </Routes>
     </BrowserRouter>
 
     // <React.Fragment></React.Fragment>
