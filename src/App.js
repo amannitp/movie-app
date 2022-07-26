7decd237c9ef1c2dce43da983ca2835c282fa301
amannitp <amannitp272@gmail.com>
2022-07-26 22:22:12 +0530

    Browser Router

--- src/App.js
+++ src/App.js
@@ -3,23 +3,17 @@ import React from "react";
 import Banner from "./components/Banner";
 import Navbar from "./components/Navbar";
 import Movielist from './components/Movielist';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 import Favourite from './components/Favourite';
 
 function App() {
   return (
+    
     <BrowserRouter>
-     <Navbar/>
-     <Routes>
-        <Route path="/" element={<>
-        <Banner/>
-        <Movielist/>
-        </>}
-
-        />
-        <Route path="/favourites" element={<Favourite/>}
-        />
-     </Routes>
+         <Navbar/>
+         <Banner/>
+         <Movielist/>
+         <Favourite/>
     </BrowserRouter>
 
     // <React.Fragment></React.Fragment>
--- src/components/Favourite.js
+++ src/components/Favourite.js
@@ -1,9 +1,67 @@
 import React, { Component } from 'react'
-
+import {movies} from '../movieData'
 export class Favourite extends Component {
+
+    
   render() {
+    let movieData=movies.results
+    let genreids = {
+        28: "Action",
+        12: "Adventure",
+        16: "Animation",
+        35: "Comedy",
+        80: "Crime",
+        99: "Documentary",
+        18: "Drama",
+        10751: "Family",
+        14: "Fantasy",
+        36: "History",
+        27: "Horror",
+        10402: "Music",
+        9648: "Mystery",
+        10749: "Romance",
+        878: "Sci-Fi",
+        10770: "TV",
+        53: "Thriller",
+        10752: "War",
+        37: "Western",
+      };
+  
     return (
-      <div><h1>hello this is my favourite</h1></div>
+      <div className='main'>
+        <div className='row'>
+            <div className='col-3'>
+            <ul class="list-group">
+  <li class="list-group-item">An item</li>
+  <li class="list-group-item">A second item</li>
+  <li class="list-group-item">A third item</li>
+  <li class="list-group-item">A fourth item</li>
+  <li class="list-group-item">And a fifth one</li>
+</ul>
+            </div>
+            <div className='col-9'>
+            <table class="table">
+  <thead>
+  </thead>
+  <tbody>
+  {
+        movieData.map((movieObj)=>{
+            <tr>
+      <th scope="row">{movieObj.original_title}</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+        })
+     }
+    
+    
+  </tbody>
+</table>
+            </div>
+        </div>
+
+      </div>
     )
   }
 }
