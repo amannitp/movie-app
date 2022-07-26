6253e8420b49fe336e27fd3b93e5008a426d2ea3
amannitp <amannitp272@gmail.com>
2022-07-26 22:50:01 +0530

    add link to the movilist and favourites

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
-
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 export class Navbar extends Component {
   render() {
     return (
       <div style={{display:'flex',alignItems:'center'}}>
         <Link to='/'><h1 >Movies App</h1></Link>
         <Link to='favourites'><h2 style={{marginLeft:"2rem",marginTop:'0.7rem'}}>Favourites</h2></Link>
+
+        {/* <a href='/'><h1 >Movies App</h1></a>
+        <a href='/favourates'><h2 style={{marginLeft:"2rem",marginTop:'0.7rem'}}>Favourites</h2></a> */}
       </div>
     )
   }
